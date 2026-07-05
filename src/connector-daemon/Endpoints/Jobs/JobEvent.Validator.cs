b29f5d28a27d0fe2791bb31067c5c76aacd118cd
AndreiLacatos <lacatos_andrei@yahoo.com>
2026-07-05 22:06:53 +0300

    Put a reverse proxy to route Jenkins webhook calls to the daemon process while any other traffic to the dashboard

--- src/connector-daemon/Endpoints/Jobs/JobEvent.Validator.cs
+++ src/connector-daemon/Endpoints/Jobs/JobEvent.Validator.cs
@@ -22,6 +22,7 @@ internal static partial class JobEvent
                 .WithMessage("Git URL must not be empty.");
 
             RuleFor(x => x.Branch)
+                .NotNull()
                 .NotEmpty()
                 .WithMessage("Branch must not be empty.");
 
--- src/connector-daemon/Persistence/Entities/JobEventEntity.cs
+++ src/connector-daemon/Persistence/Entities/JobEventEntity.cs
@@ -22,6 +22,7 @@ internal sealed class JobEventEntity
         Name = jobEvent.Name,
         Build = jobEvent.Build,
         Commit = jobEvent.Commit,
+        Branch = jobEvent.Branch,
         GitUrl = jobEvent.GitUrl,
         JobEvent = jobEvent.Status switch
         {
